469bd304f8637f8475752a5e68170bf442f1b30f
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2021-07-12 01:57:11 +0430

    Cleaned up the project a little bit and added a background

---
+++ src/components/Background.js
@@ -0,0 +1,10 @@
+const Background = () => {
+    return (
+        <div className="bg">
+            <div id="bg-color-1"></div>
+            <div id="bg-color-2"></div>
+        </div>
+    )
+}
+
+export default Background
