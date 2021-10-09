207180651a9636bbb489f35d35ff0a7b0be24977
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2021-10-09 20:28:30 +0330

    Added an empty responsive sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Background from "./components/Background";
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Background />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+    return (
+        <div className="sidebar-frame">
+            <div className="sidebar-texture">
+                <div className="sidebar-container">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
